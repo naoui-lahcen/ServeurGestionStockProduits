dec747dd02d6f514a7a03829f38864576bcc7a6c
Lahcen Naoui <lahcen.naoui@gmail.com>
2019-04-29 10:24:37 +0100

    test commit

--- src/main/java/com/dev/naoui/ServeurGestionStockProduitsApplication.java
+++ src/main/java/com/dev/naoui/ServeurGestionStockProduitsApplication.java
@@ -8,6 +8,7 @@ public class ServeurGestionStockProduitsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServeurGestionStockProduitsApplication.class, args);
+		System.out.println("Bonjour !!!!!");
 	}
 
 }
